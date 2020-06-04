6ab03c00d712f4ba77e9410340e7f5ebe5002744
ian <ian.aquino2003@gmail.com>
2020-06-04 14:48:24 -0300

    OK

--- App.tsx
+++ App.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar} from 'react-native';
+import {AppLoading} from 'expo';
+
+
+import { Roboto_500Medium, Roboto_400Regular } from '@expo-google-fonts/roboto';
+import { Ubuntu_700Bold, useFonts} from '@expo-google-fonts/ubuntu';
+
+import Routes from './src/routes';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+  const [FontsLoaded] = useFonts({
+    Roboto_500Medium,
+    Roboto_400Regular,
+    Ubuntu_700Bold,
+  });
+
+  if(!FontsLoaded){
+  return <AppLoading />
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return (
+     <>
+       <StatusBar barStyle="dark-content" translucent/>
+       <Routes />
+     </>
+  );
+}
